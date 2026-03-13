adecd2d62d46a3f3d8478c0a8157241e2c7f8002
jcardenasz <jcardenasz@unal.edu.co>
2026-03-12 21:12:33 -0500

    🚀feat: routes, navbar and footer added (not finished)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages';
+import { About } from './pages';
+import { Contact } from './pages';
+import { Projects } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  }
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,4 @@
+export { Home } from './home/home';
+export { About } from './about/about';
+export { Contact } from './contact/contact';
+export { Projects } from './projects/projects';
