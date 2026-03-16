afdd4775415296aee998db0332dbba09d6257cc2
jcardenasz <jcardenasz@unal.edu.co>
2026-03-15 22:24:10 -0500

    🚀feat: First approach to typewriting machine on its course

--- src/app/components/organisms/hero/hero.ts
+++ src/app/components/organisms/hero/hero.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero{
+export class Hero implements OnInit {
 
+  sentenceIndex = signal(0);
+  charIndex = signal(0);
+  displayText = signal('');
+  showCursor = false;
+
+  text = [
+    'My name is Juan Camilo Cardenas Zabala',
+    'I am a Fullstack Software Developer',
+    "let's forge great things together!",
+  ];
+
+  ngOnInit() {
+    this.startWriting();
+  }
+
+  startWriting() {
+    const sentence = this.text[this.sentenceIndex()];
+
+    if( this.charIndex() <= sentence.length) {
+      this.displayText.set(this.text[this.sentenceIndex()].substring(0, this.charIndex()));
+      this.charIndex.update((value) => value + 1);
+      setTimeout(() => this.startWriting(), 300);
+      console.log(this.displayText());
+    }
+  }
 }
