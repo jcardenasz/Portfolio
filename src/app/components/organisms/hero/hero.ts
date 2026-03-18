c09860b4acd440f06892b696f7a7bbf1e7127541
jcardenasz <jcardenasz@unal.edu.co>
2026-03-18 14:52:30 -0500

    🚀feat: Hero video + typewriter text done

--- src/app/components/organisms/hero/hero.ts
+++ src/app/components/organisms/hero/hero.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
 
+type Mode = 'writing' | 'deleting' | 'waiting' | 'preWriting';
+
 @Component({
   selector: 'app-hero',
   standalone: true,
@@ -11,26 +13,70 @@ export class Hero implements OnInit {
   sentenceIndex = signal(0);
   charIndex = signal(0);
   displayText = signal('');
-  showCursor = false;
+  mode = signal<Mode>('preWriting');
 
   text = [
     'My name is Juan Camilo Cardenas Zabala',
-    'I am a Fullstack Software Developer',
-    "let's forge great things together!",
+    'I am a Fullstack Software Developer living in Bogotá',
+    'Let\'s forge great things together!',
   ];
 
   ngOnInit() {
-    this.startWriting();
+    this.runTypewriter();
   }
 
-  startWriting() {
-    const sentence = this.text[this.sentenceIndex()];
-
-    if( this.charIndex() <= sentence.length) {
-      this.displayText.set(this.text[this.sentenceIndex()].substring(0, this.charIndex()));
-      this.charIndex.update((value) => value + 1);
-      setTimeout(() => this.startWriting(), 300);
-      console.log(this.displayText());
+  runTypewriter() {
+    switch (this.mode()) {
+      case 'preWriting':
+        this.pauseBeforeWriting();
+        break;
+      case 'writing':
+        this.startWriting();
+        break;
+      case 'deleting':
+        this.deleteText();
+        break;
+      case 'waiting':
+        this.wait();
+        break;
     }
+  }
+
+  pauseBeforeWriting() {
+    setTimeout(() => {
+      this.charIndex.set(0);
+      this.mode.set('writing');
+      this.runTypewriter();
+    }, 1200);
+  }
+
+  startWriting(sentence: string = this.text[this.sentenceIndex()]) {
+    if (this.charIndex() <= sentence.length) {
+      this.displayText.set(sentence.substring(0, this.charIndex()));
+      this.charIndex.update(v => v + 1);
+      setTimeout(() => this.startWriting(), 150);
+    } else {
+      this.mode.set('waiting');
+      this.runTypewriter();
+    }
+  }
+
+  deleteText(sentence: string = this.text[this.sentenceIndex()]) {
+    if (this.charIndex() >= 0) {
+      this.displayText.set(sentence.substring(0, this.charIndex()));
+      this.charIndex.update(v => v - 1);
+      setTimeout(() => this.deleteText(), 60);
+    } else {
+      this.sentenceIndex.update(v => (v + 1) % this.text.length);
+      this.mode.set('preWriting');
+      this.runTypewriter();
+    }
+  }
+
+  wait() {
+    setTimeout(() => {
+      this.mode.set('deleting');
+      this.runTypewriter();
+    }, 2500);
   }
 }
