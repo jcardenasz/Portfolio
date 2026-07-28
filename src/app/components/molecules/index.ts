273fef47ae36a28fbfe405826f93cf70a1b410de
jcardenasz <jcardenasz@unal.edu.co>
2026-07-27 23:00:44 -0500

    🚀feat: Contact page using mailto, socials molecule created to reuse code, top padding fixed in projects.

--- src/app/components/molecules/index.ts
+++ src/app/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export { Maintenance } from './maintenance/maintenance';
 export { ToolsCarousel } from './tools-carousel/tools-carousel';
 export { Values } from './values/values';
+export { Socials} from './socials/socials';
